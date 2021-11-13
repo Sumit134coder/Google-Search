214018b3a1975af694929e488a09b8400c6e64a2
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 19:19:45 +0530

    fix : Wrapping app in context provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './global.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {ResultContextProvider} from './contexts/ResultContextProvider'
 
 ReactDOM.render(
+  <ResultContextProvider>
   <Router>
     <App />
   </Router>
+  </ResultContextProvider>
  ,
   document.getElementById('root')
 );
