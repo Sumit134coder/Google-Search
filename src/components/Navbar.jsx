448bff8a900e6c70fa7b834ba9257ed6d1c10c6d
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 16:52:06 +0530

    feat : Dark mode , logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Search from './Search'
 
-const Navbar = () => {
+const Navbar = ({darkTheme, setDarkTheme}) => {
     return (
-        <div>
-            Navbar
+        <div className='p-5 pb-0 flex flex-wrap sm:justify-between justify-center item-center border-b dark:border-gray-700 border-gray-200' > 
+            <div className='flex justify-between item-center space-x-5 w-screen'>
+                <Link to="/">
+                    <p className='text-2xl bg-blue-500 rounded-lg font-bold text-white py-1 px-2 dark:bg-gray-500 dark:text-gray-900'>
+                        SearchMe🙀
+                    </p>
+                </Link>
+                <button type='button' onClick={()=>setDarkTheme(!darkTheme)}className='text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg' >
+                    {darkTheme?'Light💡':'Dark🌙'}
+                </button>
+            </div>
+            <Search/>
         </div>
     )
 }
