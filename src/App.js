26b6a665c899e3568591ef2d8bda9e4b7640398c
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 15:47:20 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React,{useState} from "react";
+import './global.css'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import{ Routes} from "./components/Routes";
+
+
+
 
 function App() {
+
+  const [darkTheme , setDarkTheme] = useState(false)
+
   return (
-    <div className="App">
-     hello
+    <div className={darkTheme?'dark':""} >
+      <div className='bg-gray-200 dark:bg-gray-900 dark:text-white min-h-screen'>
+        <Navbar/>
+        <Routes/>
+        <Footer/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Routes.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Route,Switch} from 'react-router-dom'
+
+export const Routes = () => {
+    return (
+        <div>
+            <h1>Routes</h1>
+        </div>
+    )
+}
