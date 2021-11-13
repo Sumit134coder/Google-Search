5b84fe730605b458e448ae131a24e63c95a8c173
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 20:10:57 +0530

    feat : News section working

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -61,7 +61,27 @@ const Results = () => {
                 </div>
             )       
         case '/news':
-            return 'search';
+            return (
+                <div className='flex flex-wrap justify-between space-y-6 sm:px-56 items-center'>
+                {results?.entries?.map(({links,id,source,title} )=>(
+                    <div key={id} className='md:w-2/5 w-full'>
+                        <a href={links?.[0].href} target='_blank'
+                        rel='noreferrer'
+                        className='hover:underline'>
+                            <p className='text-lg dark:text-blue-300 text-blue-700'>
+                                {title}
+                            </p>
+                            <div className='flex gap-4'>
+                                <a href={source?source.href:''} target='_blank' rel='noreferrer'>
+                                    {source?.href}
+                                </a>
+                            </div>
+
+                        </a>
+                    </div>
+                ))}
+            </div>
+            )
         case '/videos':
             return 'search';
         default:
