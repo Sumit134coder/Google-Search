302a17dac2a3724228212823fd6ff0551fde18c5
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 17:23:59 +0530

    Feat : React router v6 routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import {Route,Switch} from 'react-router-dom'
+import {Route , Routes as Switch,Navigate } from 'react-router-dom'
+import Results from './Results'
 
 export const Routes = () => {
     return (
-        <div>
-            <h1>Routes</h1>
+        <div className='p-4'>
+           <Switch>
+                <Route  path="/images" exact element={<Results/>} />
+                <Route  path="/videos" exact element={<Results/>} />
+                <Route  path="/news" exact element={<Results/>} />
+                <Route  path="/search" exact element={<Results/>} />
+                <Route path="/" element={<Navigate replace to="/search" />} />
+                    
+               
+           </Switch>
+           routes
         </div>
     )
 }
