84007d5c72616054636905bb5dec7d1b7dc36d5f
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 19:08:48 +0530

    ADD : Loader

---
+++ src/components/Loading.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Loader from 'react-loader-spinner'
+
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center">
+           <Loader
+               type="Plane"
+               color="#00BFFF"
+               height={550}
+               width={80}
+           />
+        </div>
+    )
+}
+
+export default Loading
