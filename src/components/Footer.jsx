58364e8d1e6fce920666d24a2df08a2f09a45cdf
Sumit134coder <73213318+Sumit134coder@users.noreply.github.com>
2021-11-13 17:28:51 +0530

    feat : Footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="border-t border-gray-200 text-center p-3  text-sm mt-10 dark:border-gray-700">
+            <h1>
+                 All Rights reserved
+            </h1>
+
         </div>
     )
 }
